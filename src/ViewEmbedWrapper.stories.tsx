560503b0217253e8080722ed2ad75cc15cfc273e
Shane Jeffery <shane@formant.io>
2024-09-16 23:54:29 -0700

    DRY and move auth logic to hook

--- src/ViewEmbedWrapper.stories.tsx
+++ src/ViewEmbedWrapper.stories.tsx
@@ -9,6 +9,27 @@ import "react-calendar/dist/Calendar.css";
 import "react-clock/dist/Clock.css";
 import "react-datetime-picker/dist/DateTimePicker.css";
 
+const API_BASE_URL = "https://api.formant.io";
+const AUTH_LOGIN_ENDPOINT = "/v1/admin/auth/login-embed";
+const SERVICE_ACCOUNT_EMAIL =
+  "embed@3e3fa599-37a2-4c64-916d-e27e9fb370ee.iam.formant.io";
+const SERVICE_ACCOUNT_PASSWORD =
+  "42q3WEBGsUbKRTsU8ifmBC0HxV71i7rSrowL_ISu3px4lo7QQOpPr_fCXsQb0_zP";
+const DEVICE_OPTIONS = [
+  { value: "9fccbfd0-67e8-47c9-be7a-10105a737050", label: "Holman View Embed" },
+];
+
+const TIME_RANGE_OPTIONS = [
+  { value: "", label: "Custom Range" },
+  { value: "3 hour", label: "3 hours" },
+  { value: "1 hour", label: "1 hour" },
+  { value: "30 minute", label: "30 minutes" },
+  { value: "5 minute", label: "5 minutes" },
+  { value: "1 minute", label: "1 minute" },
+  { value: "30 seconds", label: "30 seconds" },
+  { value: "5 seconds", label: "5 seconds" },
+];
+
 const meta = {
   title: "View Embed Wrapper",
   component: ViewEmbedWrapper,
@@ -22,7 +43,6 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 type ValuePiece = Date | null;
-
 type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 interface IProps {
@@ -32,56 +52,39 @@ interface IProps {
   fontFamilyUrl?: string;
 }
 
+const axiosInstance = axios.create({
+  baseURL: API_BASE_URL,
+  withCredentials: false,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "POST",
+  },
+});
+
 const provisionAuthToken = async () => {
-  const instance = axios.create({
-    baseURL: "https://api.formant.io",
-    withCredentials: false,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "POST",
-    },
+  const response = await axiosInstance.post(AUTH_LOGIN_ENDPOINT, {
+    email: SERVICE_ACCOUNT_EMAIL,
+    password: SERVICE_ACCOUNT_PASSWORD,
   });
-
-  const serviceAccountEmail =
-    "embed@3e3fa599-37a2-4c64-916d-e27e9fb370ee.iam.formant.io";
-  const serviceAccountPassword =
-    "42q3WEBGsUbKRTsU8ifmBC0HxV71i7rSrowL_ISu3px4lo7QQOpPr_fCXsQb0_zP";
-
-  const response = await instance.post("/v1/admin/auth/login-embed", {
-    email: serviceAccountEmail,
-    password: serviceAccountPassword,
-  });
-
   return response.data.accessToken;
 };
 
-const EmbedWithHooks = (props: IProps) => {
-  const { deviceId, viewId, themeOverride, fontFamilyUrl } = props;
+const useAuthToken = () => {
+  const [authToken, setAuthToken] = useState<string>("");
 
-  const [selectedDeviceId, setSelectedDeviceId] = useState(deviceId);
-  const [selectedDate, setSelectedDate] = useState<Value>(new Date());
-  const [selectedTimeRange, setSelectedTimeRange] = useState("");
-  const [authToken, setAuthToken] = useState("");
+  const fetchAuthToken = async () => {
+    const token = await provisionAuthToken();
+    setAuthToken(token);
+  };
 
   useEffect(() => {
-    const fetchAuthToken = async () => {
-      const tempAuthToken = await provisionAuthToken();
-      setAuthToken(tempAuthToken);
-    };
-
     fetchAuthToken();
   }, []);
 
   useEffect(() => {
-    const checkTokenExpiration = async () => {
-      const refreshToken = async () => {
-        const newToken = await provisionAuthToken();
-        setAuthToken(newToken);
-      };
-
+    const checkTokenExpiration = () => {
       const delay = 60000;
       const timer = setInterval(() => {
-        console.log(":: Checking token expiration");
         const currentDtm = Math.round(new Date().getTime() / 1000);
 
         if (authToken) {
@@ -89,9 +92,8 @@ const EmbedWithHooks = (props: IProps) => {
             const decodedToken: { exp: number } = jwtDecode(authToken);
 
             if (decodedToken.exp && currentDtm >= decodedToken.exp) {
-              console.log(":: Token expired, fetching new token");
               clearInterval(timer);
-              refreshToken();
+              fetchAuthToken();
             }
           } catch (error) {
             console.error(":: Error decoding token", error);
@@ -107,6 +109,30 @@ const EmbedWithHooks = (props: IProps) => {
     }
   }, [authToken]);
 
+  return authToken;
+};
+
+const Dropdown = ({ options, onChange, defaultValue }: any) => (
+  <select onChange={onChange} defaultValue={defaultValue}>
+    {options.map((option: { value: string; label: string }) => (
+      <option key={option.value} value={option.value}>
+        {option.label}
+      </option>
+    ))}
+  </select>
+);
+
+const EmbedWithHooks = ({
+  deviceId,
+  viewId,
+  themeOverride,
+  fontFamilyUrl,
+}: IProps) => {
+  const [selectedDeviceId, setSelectedDeviceId] = useState(deviceId);
+  const [selectedDate, setSelectedDate] = useState<Value>(new Date());
+  const [selectedTimeRange, setSelectedTimeRange] = useState("");
+  const authToken = useAuthToken();
+
   return (
     <>
       <div
@@ -119,15 +145,10 @@ const EmbedWithHooks = (props: IProps) => {
       >
         <div style={{ paddingBottom: "10px", paddingTop: "5px" }}>
           Choose Device:{" "}
-          <select
-            onChange={(event) => {
-              setSelectedDeviceId(event.currentTarget.value);
-            }}
-          >
-            <option value="9fccbfd0-67e8-47c9-be7a-10105a737050">
-              Holman View Embed
-            </option>
-          </select>
+          <Dropdown
+            options={DEVICE_OPTIONS}
+            onChange={(e: any) => setSelectedDeviceId(e.currentTarget.value)}
+          />
         </div>
 
         <div style={{ paddingBottom: "10px" }}>
@@ -137,36 +158,25 @@ const EmbedWithHooks = (props: IProps) => {
 
         <div style={{ paddingTop: "5px" }}>
           Choose Time Range:{" "}
-          <select
+          <Dropdown
+            options={TIME_RANGE_OPTIONS}
+            onChange={(e: any) => setSelectedTimeRange(e.currentTarget.value)}
             defaultValue="30 minute"
-            onChange={(event) => {
-              setSelectedTimeRange(event.currentTarget.value);
-            }}
-          >
-            <option value="">Custom Range</option>
-            <option value="3 hour">3 hours</option>
-            <option value="1 hour">1 hour</option>
-            <option value="30 minute">30 minutes</option>
-            <option value="5 minute">5 minutes</option>
-            <option value="1 minute">1 minute</option>
-            <option value="30 seconds">30 seconds</option>
-            <option value="5 seconds">5 seconds</option>
-          </select>
+          />
         </div>
       </div>
-      <div>
-        <ViewEmbedWrapper
-          fontFamilyUrl={fontFamilyUrl}
-          viewId={viewId}
-          deviceId={selectedDeviceId}
-          authToken={authToken}
-          // @ts-ignore
-          currentDate={selectedDate}
-          timeRange={selectedTimeRange}
-          themeOverride={themeOverride}
-          dataSrcUrl="http://localhost:5174"
-        />
-      </div>
+
+      <ViewEmbedWrapper
+        fontFamilyUrl={fontFamilyUrl}
+        viewId={viewId}
+        deviceId={selectedDeviceId}
+        authToken={authToken}
+        // @ts-ignore
+        currentDate={selectedDate}
+        timeRange={selectedTimeRange}
+        themeOverride={themeOverride}
+        dataSrcUrl="http://localhost:5174"
+      />
     </>
   );
 };
@@ -179,7 +189,7 @@ export const Base: Story = {
   },
   render: () => (
     <EmbedWithHooks
-      deviceId="9fccbfd0-67e8-47c9-be7a-10105a737050"
+      deviceId={DEVICE_OPTIONS[0].value}
       viewId="9140bb02-32fe-47ea-bc24-8f6178eff205"
       fontFamilyUrl="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap"
       themeOverride={{
