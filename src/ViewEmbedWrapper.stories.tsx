1c10c3dac0f0ee143fa99f73923767d03bd55b63
Shane Jeffery <shane@formant.io>
2024-09-18 01:28:28 -0700

    Setting up some base demos

--- src/ViewEmbedWrapper.stories.tsx
+++ src/ViewEmbedWrapper.stories.tsx
@@ -4,6 +4,10 @@ import React, { useState } from "react";
 import DateTimePicker from "react-datetime-picker";
 import { ViewEmbedWrapper } from "./ViewEmbedWrapper";
 
+import Img1 from "./assets/img1.png";
+import Img2 from "./assets/img2.png";
+import Logo from "./assets/logo.png";
+
 import "react-calendar/dist/Calendar.css";
 import "react-clock/dist/Clock.css";
 import "react-datetime-picker/dist/DateTimePicker.css";
@@ -47,6 +51,8 @@ type ValuePiece = Date | null;
 type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 interface IProps {
+  pageLayoutAboveContent?: any;
+  pageLayoutBelowContent?: any;
   viewId: string;
   deviceId: string;
   themeOverride: Record<string, any>;
@@ -64,6 +70,8 @@ const Dropdown = ({ options, onChange, defaultValue }: any) => (
 );
 
 const EmbedWithHooks = ({
+  pageLayoutAboveContent,
+  pageLayoutBelowContent,
   deviceId,
   viewId,
   themeOverride,
@@ -81,6 +89,8 @@ const EmbedWithHooks = ({
 
   return (
     <>
+      {pageLayoutAboveContent}
+
       <div
         style={{
           paddingBottom: "20px",
@@ -123,11 +133,13 @@ const EmbedWithHooks = ({
         themeOverride={themeOverride}
         dataSrcUrl="http://localhost:5174"
       />
+
+      {pageLayoutBelowContent}
     </>
   );
 };
 
-export const Base: Story = {
+export const BaseDemo: Story = {
   args: {
     viewId: "",
     deviceId: "",
@@ -135,6 +147,61 @@ export const Base: Story = {
   },
   render: () => (
     <EmbedWithHooks
+      deviceId={DEVICE_OPTIONS[0].value}
+      viewId="9140bb02-32fe-47ea-bc24-8f6178eff205"
+      fontFamilyUrl="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap"
+      themeOverride={{
+        colors: {
+          "formant-color-primary-white": "#000000",
+          "formant-color-primary-silver": "#1F1F1F",
+          "formant-color-primary-black": "#FFFFFF",
+          "formant-color-primary-dark": "#E6E6E6",
+          "formant-color-primary-flagship": "#E8E8E8",
+          "formant-color-background": "#E8E8E8",
+          "formant-color-primary-module": "#FFFFFF",
+          "formant-color-primary-steel01": "#005897",
+          "formant-color-primary-steel02": "#005897",
+          "formant-color-primary-steel03": "#81cbff",
+          "formant-color-highlights-blue": "#005897",
+          "formant-color-data-royal-blue": "#005897",
+          "formant-color-data-purple": "#E6E6FA",
+          "formant-color-data-red": "#FF7F50",
+          "formant-color-data-orange": "#FFDAB9",
+          "formant-color-data-yellow": "#F9C36E",
+          "formant-color-data-green": "#40E0D0",
+        },
+        fontFamily: "Oswald",
+      }}
+    />
+  ),
+};
+
+export const HolmanDemo: Story = {
+  args: {
+    viewId: "",
+    deviceId: "",
+    authToken: "",
+  },
+  render: () => (
+    <EmbedWithHooks
+      pageLayoutAboveContent={
+        <>
+          <div>
+            <img src={Logo} />
+          </div>
+
+          <div style={{ paddingBottom: "10px" }}>
+            <img src={Img1} width="100%" />
+          </div>
+        </>
+      }
+      pageLayoutBelowContent={
+        <>
+          <div style={{ paddingTop: "10px" }}>
+            <img src={Img2} width="100%" />
+          </div>
+        </>
+      }
       deviceId={DEVICE_OPTIONS[0].value}
       viewId="9140bb02-32fe-47ea-bc24-8f6178eff205"
       fontFamilyUrl="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap"
