f7a1c1573c4272df3da9caa805768f5d0707bbf5
Shane Jeffery <shane@formant.io>
2024-11-12 14:14:02 -0800

    Updates

---
+++ src/ViewEmbedWrapper.d.ts
@@ -0,0 +1,24 @@
+import { default as React } from '../node_modules/react';
+import { ISimpleUserScope } from '../model/ISimpleUserScope';
+interface IProps {
+    hasAuthToken?: boolean;
+    serviceAccountEmail?: string;
+    serviceAccountPassword?: string;
+    authScope?: ISimpleUserScope;
+    apiBaseUrl?: string;
+    viewId?: string;
+    deviceIds?: string[];
+    authToken?: string;
+    dataSrcUrl?: string;
+    viewTags?: any;
+    currentDate?: Date;
+    aggregation?: string;
+    aggregateStartDate?: Date;
+    aggregateEndDate?: Date;
+    timeRange?: string;
+    themeOverride?: any;
+    wrapperStyleOverride?: any;
+    fontFamilyUrl?: string;
+}
+export declare const ViewEmbedWrapper: (props: IProps) => React.JSX.Element;
+export {};
