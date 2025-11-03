46c9bad7796b1ecfab8478234e61c397d63003e3
AlbertVosk <albert.voskaniann@gmail.com>
2025-11-03 17:18:43 +0400

    Revert "Embed updates"

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,13 +1,1 @@
 /// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-  readonly VITE_STORYBOOK_AUTH_TOKEN?: string;
-  readonly VITE_STORYBOOK_VIEW_ID?: string;
-  readonly VITE_STORYBOOK_DEVICE_ID?: string;
-  readonly VITE_STORYBOOK_API_BASE_URL?: string;
-  readonly VITE_STORYBOOK_DATA_SRC_URL?: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
